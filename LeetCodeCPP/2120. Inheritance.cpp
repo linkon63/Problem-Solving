0d30e7759e0ef7b972516229f174a6779af75c2a
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2022-03-21 23:49:30 +0600

    1720 CPP

--- LeetCodeCPP/2120. Inheritance.cpp
+++ LeetCodeCPP/2120. Inheritance.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
-
 using namespace std;
+
+class A
+{
+public:
+    int a = 10,b = 20;
+    void printA()
+    {
+        cout<<"A + B = "<< a+b<<endl;
+    }
+};
+
+class B:public A
+{
+public:
+    // int a = 30;
+    void printB()
+    {
+        cout<<"A + B = "<< a+b<<endl;
+    }
+};
 
 int main()
 {
-    cout << "Inheritance ";
+    A aObj;
+    B bObj;
+    cout<<"A Class Proparties : "<<endl;
+    cout<<"A:"<<aObj.a<<endl;
+    cout<<"B:"<<aObj.b<<endl;
+    aObj.printA();
+
+    cout<<"Inheritance Class Proparties : "<<endl;
+    cout<<"A:"<<bObj.a<<endl;
+    cout<<"B:"<<bObj.b<<endl;
+    bObj.printB();
+    bObj.printA();
+
     return 0;
 }
